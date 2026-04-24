8dea8da4a189c0f75cfacc681b419537cdf9c11c
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-04-24 23:29:58 +0100

    Created side bar component and added functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,27 @@
+import { useState } from 'react'
+import { Sidebar } from './Sidebar'
 import logo from '../assets/shared/logo.svg'
 import hamburger from '../assets/shared/icon-hamburger.svg'
-
-
 export function Header(){
-    
-    return(
-        <header className='w-full grid py-10'>
-            <nav className='w-5/6 mx-auto flex justify-between items-center'>
-                <div>
-                    <img src={logo} alt="logo icon" />
-                </div>
+    const [menuIsVisible,setMenuIsVisible]=useState(false)
 
-                <div>   
-                    <img src={hamburger} alt="hamburger icon" width={30}/>
-                </div>
-            </nav>
-        </header>
+    function toggleMenu(){
+        setMenuIsVisible(prev=>!prev)
+    }
+    return(
+        <>
+            <Sidebar menuIsVisble={menuIsVisible} setMenuIsVisible={setMenuIsVisible}/>
+            <header className='w-full grid py-10'>
+                <nav className='w-5/6 mx-auto flex justify-between items-center'>
+                    <div>
+                        <img src={logo} alt="logo icon" />
+                    </div>
+
+                    <button onClick={toggleMenu}>   
+                        <img src={hamburger} alt="hamburger icon" width={30}/>
+                    </button>
+                </nav>
+            </header>
+        </>
     )
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,39 @@
+import close from "../assets/shared/icon-close.svg";
+import { Link } from "react-router-dom";
+export function Sidebar({menuIsVisble,setMenuIsVisible}:any) {
+    function closeMenu(){
+        setMenuIsVisible(false)
+    }
+  return (
+    <nav
+        className={`fixed top-0 ${menuIsVisble?'right-0':'-right-80'} h-screen w-[70%]
+        bg-gradient-to-br from-black/70 via-slate-900/50 to-neutral-200/10
+        backdrop-blur-2xl
+        border-l border-white/10
+        shadow-2xl text-white pt-10 flex flex-col gap-10 ease-in duration-300`}
+    >
+
+      <button onClick={closeMenu} className="flex justify-end font-bellefair">
+        <img src={close} alt="close-icon" />
+      </button>
+      <ul className="mt-16 grid gap-7 pl-5">
+        <Link to={'/'} className="flex gap-2 text-xl border-r-4">
+          <span className="text-White font-bold">00</span>
+          HOME
+        </Link>
+        <Link to={'/destination'} className="flex gap-2 text-xl hover:border-r-4">
+          <span className="text-White font-bold">01</span>
+          DESTINATION
+        </Link>
+        <Link to={'/crew'} className="flex gap-2 text-xl hover:border-r-4">
+          <span className="text-White font-bold">02</span>
+          CREW
+        </Link>
+        <Link to={'/technology'} className="flex gap-2 text-xl hover:border-r-4">
+          <span className="text-White font-bold">03</span>
+          TECHNOLOGY
+        </Link>
+      </ul>
+    </nav>
+  );
+}
