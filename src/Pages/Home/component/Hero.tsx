ef7b1918aed3e926a0ba2521d75b6aa5842a3908
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-05-02 10:44:43 +0100

    install gh pages package

--- src/Pages/Home/component/Hero.tsx
+++ src/Pages/Home/component/Hero.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
 export function Hero(){
     return(
         <section className="w-full grid py-10 gap-10 content-between lg:grid-cols-2 lg:w-5/6 lg:mx-auto lg:h-screen lg:content-end ">
@@ -12,7 +12,7 @@ export function Hero(){
                 </p>
             </div>
             <div className="lg:justify-self-end h-96 flex justify-center items-center w-96 rounded-full hover:bg-white/20 ease-in duration-300 md:justify-self-center">
-                <button className="my-20 justify-self-center w-48 h-48 lg:w-64 lg:h-64 rounded-full bg-White text-2xl lg:text-3xl font-bellefair ">
+                <button className="my-20 justify-self-center w-48 h-48 lg:w-64 lg:h-64 rounded-full bg-White text-2xl lg:text-3xl font-bellefair" >
                     <Link to={'/destination'}>EXPLORE</Link>
                 </button>
             </div>
